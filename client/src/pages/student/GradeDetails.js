21956798d00ca28bb21c6dfbaa27e9416bdca6d6
REPHOANG <hoangpdhe176197@fpt.edu.vn>
2026-04-23 09:32:22 +0700

    Add MessageSquare icon import to GradeDetails component

--- client/src/pages/student/GradeDetails.js
+++ client/src/pages/student/GradeDetails.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
+import { MessageSquare } from "lucide-react";
 import {
   Radar,
   RadarChart,
